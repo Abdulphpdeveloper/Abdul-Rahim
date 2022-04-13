f58861c711789910698ea0303dc50a173dff4193
Abdul <abdulrahim@trioangle.com>
2022-04-13 16:12:46 +0530

    intial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <>
+      <p>Learn React</p>
+      <ExpenseItem></ExpenseItem>
+    </>
+  );
+}
+
+export default App;
