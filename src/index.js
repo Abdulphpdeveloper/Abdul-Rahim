ce65fe64306a0b1406d1803e5ae9ef491ed9adc7
Abdul <abdulrahim@trioangle.com>
2022-04-19 18:09:28 +0530

    omit react from react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
@@ -10,7 +9,9 @@ import App from "./App";
   document.getElementById("root")
 );
 */
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+//ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
