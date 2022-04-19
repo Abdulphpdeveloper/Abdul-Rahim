62ba5283e09cf4f3e1f55884335bed7b2d7db6b3
Abdul <abdulrahim@trioangle.com>
2022-04-19 15:30:12 +0530

    seperate components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 // we no need to call the passed parameter with the same name instead we can access as object
 
 // here props will be equal to title,amount,date
 const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
